56caca3e0eff943b55d93a442081634987a7f656
YogiSly <yogisly44@gmail.com>
2021-11-25 23:16:19 +0300

    feature(modal.js): feature in animate

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,42 +1,22 @@
-import { modalAnimation } from "../modules/helpers.js";
+import { animate } from "./helpers.js";
 const modal = () => {
   const modalPop = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
-  modalAnimation();
+
   modalPop.style.opacity = 0;
-
-  const showModal = () => {
-    modalPop.style.opacity = 0;
-    const show = setInterval(() => {
-      if (modalPop.style.opacity < 1) {
-        modalPop.style.opacity =
-          Math.round((+modalPop.style.opacity + 0.01) * 100) / 100;
-      }
-      if (modalPop.style.opacity >= 1) {
-        modalPop.style.opacity = 1;
-        clearInterval(show);
-      }
-    }, 5);
-  };
-
-  const hideModal = () => {
-    const hide = setInterval(() => {
-      if (modalPop.style.opacity > 0) {
-        modalPop.style.opacity =
-          Math.round((modalPop.style.opacity - 0.01) * 100) / 100;
-      }
-      if (modalPop.style.opacity <= 0) {
-        modalPop.style.opacity = 0;
-        clearInterval(hide);
-        modalPop.style.display = "none";
-      }
-    }, 5);
-  };
 
   buttons.forEach((btns) => {
     btns.addEventListener("click", () => {
       modalPop.style.display = "block";
-      showModal();
+      animate({
+        duration: 700,
+        timing(timeFraction) {
+          return Math.pow(timeFraction, 5);
+        },
+        draw(progress) {
+          modalPop.style.opacity = progress;
+        },
+      });
     });
   });
 
@@ -45,7 +25,18 @@ const modal = () => {
       !e.target.closest(".popup-content") ||
       e.target.classList.contains("popup-close")
     ) {
-      hideModal();
+      animate({
+        duration: 550,
+        timing(timeFraction) {
+          return Math.pow(timeFraction, 5);
+        },
+        draw(progress) {
+          modalPop.style.opacity = 1 - progress;
+        },
+      });
+      setTimeout(() => {
+        modalPop.style.display = "none";
+      }, 550);
     }
   });
 };
