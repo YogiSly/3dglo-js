789565e07ca0caacffc005a09a59228a8746f700
YogiSly <yogisly44@gmail.com>
2021-11-25 05:46:15 +0300

    feature (slider.js): feature in slider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
 import regular from "./modules/regular.js";
 import tabs from "./modules/tabs.js";
+import slider from "./modules/slider.js";
 
 timer("06 december 2021");
 menu();
 modal();
 regular();
 tabs();
+slider();
